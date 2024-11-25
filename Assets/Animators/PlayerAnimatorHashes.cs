a5207122cbecb33c50fc010ca16f941acacbe70d
Jonathan Ong <jko665_000@msn.com>
2024-11-25 15:34:08 -0500

    Animator Hashes to avoid string literals

---
+++ Assets/Animators/PlayerAnimatorHashes.cs
@@ -0,0 +1,7 @@
+using UnityEngine;
+
+public class PlayerAnimatorHashes
+{
+    public static readonly int IsWalking = Animator.StringToHash("IsWalking");
+    //public static readonly int Jump = Animator.StringToHash("Jump");
+}
