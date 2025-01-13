c9c022789b50480dac8ac61576ff5350e2c466c6
Jonathan Ong <jko665_000@msn.com>
2025-01-12 20:12:02 -0500

    Update to meet interaction manager method update; refactor

--- Assets/Scripts/PlayerInteractController.cs
+++ Assets/Scripts/PlayerInteractController.cs
@@ -1,31 +1,56 @@
+using System;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class PlayerInteractController : MonoBehaviour
 {
     [SerializeField] private InputReader _inputReader;
+    [SerializeField] private InteractionManager _interactManager;
     [SerializeField] private BoxCollider _collider;
-    [SerializeField] private FloatReference _selectDistance;
+    [SerializeField] private Transform _playerVisual;
+    [SerializeField] private FloatReference _interactRange;
+    private bool _isInRange = false;
 
     private Vector3 _playerVisualCenter;
+    private Vector3 _inputMoveDirection;
 
-    private void Start()
+    private void Awake()
     {
         _collider = GetComponent<BoxCollider>();
+        _playerVisual = transform.GetChild(0);
     }
 
     private void Update()
     {
-        _playerVisualCenter = transform.TransformPoint(_collider.center);
+        _playerVisualCenter = _playerVisual.TransformPoint(_collider.center);
     }
 
     private void FixedUpdate()
     {
-        Debug.Log(Physics.Raycast(_playerVisualCenter, transform.forward, out RaycastHit hitInfo, _selectDistance.Value));
+        _isInRange = _interactManager.CheckInRangeToInteract(_collider, _playerVisual, _interactRange);
+    }
+
+    private void OnMoveInput(Vector2 vector)
+    {
+        _inputMoveDirection = new Vector3(vector.x, 0f, vector.y);
     }
 
     private void OnDrawGizmos()
     {
-        Gizmos.color = Color.red;
-        Gizmos.DrawLine(_playerVisualCenter, _playerVisualCenter + transform.forward * _selectDistance.Value);
+        if (_isInRange) Gizmos.color = Color.green;
+        else Gizmos.color = Color.red;
+        if (_playerVisual != null) 
+            Gizmos.DrawLine(transform.TransformPoint(_collider.center), transform.TransformPoint(_collider.center) + _playerVisual.forward * _interactRange.Value);
     }
+
+    private void OnEnable()
+    {
+        _inputReader.MovePerformed += OnMoveInput;
+    }
+
+    private void OnDisable()
+    {
+        _inputReader.MovePerformed -= OnMoveInput;
+    }
+
 }
